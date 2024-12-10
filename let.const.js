4cad681a2c684167a057cb79ad9b66d6e9d842ac
Mayur Bhuran <bhuranmayur20@gmail.com>
2024-12-10 19:26:34 +0530

    variables done

---
+++ let.const.js
@@ -0,0 +1,16 @@
+const accountId = 13435
+let accountEmail = "mayurrb@123gmail.com"
+var accountPassword ="24232"
+accountCity ="Mumbai"
+let accountState ;
+
+//acoountId = 5668 // not allowd
+
+accountEmail ="rohit@gamil.com"
+accountPassword ="11312"
+accountCity = "channei"
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState ])
