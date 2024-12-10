b6fddc280a972ff30436167f2bf52b91bee274c7
Mayur Bhuran <bhuranmayur20@gmail.com>
2024-12-10 19:20:16 +0530

    variables

---
+++ conversion.opertion.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number(score)
+
+console.log(typeof valueinnumber )
+console.log(valueinnumber)
+
+//"43" => 33
+//"43jhhj"=> Nan
+//true=1  false=0
+let isloggeodIn ="dany"
+
+let booleanisloggeodIn = Boolean(isloggeodIn)
+console.log(booleanisloggeodIn)
+
+//1=true 0=false
+//""= false 
+
+let someNumber = 454
+
+let stringNumber =String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat as js code as newer version
+
+// alert 3+3 ;// we  are using node js not browser
+
+
+let name = "hari"
+let age = 32
+let ufilltheform = true
+
+//primitive datatype 
+ 
+//number  2to power 53
+//bigint
+//string ""
+// boolean yes or no
+//null
+
+//object 
+
+console.log(typeof null);
+
